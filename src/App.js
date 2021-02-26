b79bc9c59781c4127b418ab7111a23cbd646e3e8
Cyrus <cyruslee487@gmail.com>
2021-02-25 19:29:05 -0800

    typed-text/header/particles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 
 import './App.css';
 import NavBar from "./components/NavBar";
+import Header from "./components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from 'react-particles-js'
 
 function App() {
   return (
-    <NavBar />
+    <>
+      <Particles
+        className='particles-canvas'
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 6,
+                color: 'f9ab00'
+              },
+            }
+          }
+        }}
+      />
+      <NavBar />
+      <Header />
+    </>
   );
 }
 
