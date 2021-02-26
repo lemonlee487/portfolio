1727b48dd431bfccba6d62a8b3019fe49ca6ee32
Cyrus <cyruslee487@gmail.com>
2021-02-25 18:18:48 -0800

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import NavBar from "./components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <h1>MY PORTFOLIO</h1>
+    <NavBar />
   );
 }
 
